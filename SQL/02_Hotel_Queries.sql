dc5017ad00183550757c34e0d16c1f4f8f4bbc5a
bhuvanadasari26 <bhuvanadasari26@gmail.com>
2026-04-08 17:13:30 +0530

    Updated with assignment Q1

--- SQL/02_Hotel_Queries.sql
+++ SQL/02_Hotel_Queries.sql
@@ -2,50 +2,17 @@
 -- HOTEL MANAGEMENT SYSTEM QUERIES
 -- ===============================
 
--- Q1: Find the latest booking
-SELECT *
-FROM bookings
-ORDER BY booking_date DESC
-LIMIT 1;
-
-
--- Q2: Calculate total billing per booking
+-- Q1: Last booked room for each user
 SELECT 
-    b.booking_id,
-    SUM(i.rate * bc.quantity) AS total_bill
+    b.user_id,
+    b.room_no
 FROM bookings b
-JOIN booking_commercials bc 
-    ON b.booking_id = bc.booking_id
-JOIN items i 
-    ON bc.item_id = i.item_id
-GROUP BY b.booking_id;
-
-
--- Q3: Find bookings with total bill > 1000
-SELECT 
-    b.booking_id,
-    SUM(i.rate * bc.quantity) AS total_bill
-FROM bookings b
-JOIN booking_commercials bc 
-    ON b.booking_id = bc.booking_id
-JOIN items i 
-    ON bc.item_id = i.item_id
-GROUP BY b.booking_id
-HAVING total_bill > 1000;
-
-
--- Q4: Find the 2nd highest bill
-SELECT booking_id, total_bill
-FROM (
+JOIN (
     SELECT 
-        b.booking_id,
-        SUM(i.rate * bc.quantity) AS total_bill
-    FROM bookings b
-    JOIN booking_commercials bc 
-        ON b.booking_id = bc.booking_id
-    JOIN items i 
-        ON bc.item_id = i.item_id
-    GROUP BY b.booking_id
+        user_id, 
+        MAX(booking_date) AS last_booking
+    FROM bookings
+    GROUP BY user_id
 ) t
-ORDER BY total_bill DESC
-LIMIT 1 OFFSET 1;
+ON b.user_id = t.user_id 
+AND b.booking_date = t.last_booking;
